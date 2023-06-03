2f67b62621b8bd833f76fee97438162cfef6443a
Guilherme Monteiro de Oliveira <monteiro.oliveira1@outlook.com>
2023-06-03 19:47:49 +0000

    Test Lexer analyzer, token list output

--- src/com/mcpl/main/Main.java
+++ src/com/mcpl/main/Main.java
@@ -1,7 +1,13 @@
 package com.mcpl.main;
 
+import com.mcpl.lexer.*;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args){
-	System.out.println("Hello world");
+	var source = "9+99or and 44 lever: 32 int asdad";
+	Lexer lexer = new Lexer(source);
+	List<Token> tokens = lexer.analyze();
+	System.out.println(tokens.toString());
     }
 }
