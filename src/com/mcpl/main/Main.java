3c8b5b2b8013af47d784b4d6e5890c986c0cdd29
Guilherme Monteiro de Oliveira <monteiro.oliveira1@outlook.com>
2023-06-04 12:38:34 -0300

    Test syntactical analyzer

--- src/com/mcpl/main/Main.java
+++ src/com/mcpl/main/Main.java
@@ -1,13 +1,15 @@
 package com.mcpl.main;
 
 import com.mcpl.lexer.*;
+import com.mcpl.syntactic.*;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args){
-	var source = "9+99or and 44 lever: 32 int asdad";
+	var source = "lever: djsdjsdkjsd";
 	Lexer lexer = new Lexer(source);
 	List<Token> tokens = lexer.analyze();
-	System.out.println(tokens.toString());
+	Syntactic syn = new Syntactic(tokens);
+	syn.analyze();
     }
 }
